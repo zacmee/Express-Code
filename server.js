7f4e2e32fc1ce6ff2c09f7c008da9fa20468f0e6
Pankaj <pankajbskbisht@yahoo.co.in>
2021-02-22 22:57:22 +0530

    Serving Static Assets

--- server.js
+++ server.js
@@ -21,6 +21,9 @@ if (!process.env.DISABLE_XORIGIN) {
   });
 }
 
+app.use(express.static(__dirname, + "/public"));
+app.use("/public", express.static(__dirname, + "/public"));
+
 var port = process.env.PORT || 3000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function(){
   bGround.log('Node is listening on port '+ port + '...')
