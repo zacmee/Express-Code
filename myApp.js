125506e2be858e036e2783ccb367905f1e03c810
Pankaj <pankajbskbisht@yahoo.co.in>
2021-02-22 21:00:11 +0530

    serve HTML

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ var app = express();
 console.log("Hello World");
 
 app.get("/", (req, res) => {
-    res.send(__dirname + "/views/index.html")
+    res.sendFile(__dirname + "/views/index.html");
 });
 
 
